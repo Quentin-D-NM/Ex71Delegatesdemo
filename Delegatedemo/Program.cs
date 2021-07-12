744735d332e1e876b7a511554e2838e9eb22fff6
Quentin-D-NM <qdye2001@gmail.com>
2021-07-12 14:51:30 -0600

    Initial Commit

--- Delegatedemo/Program.cs
+++ Delegatedemo/Program.cs
@@ -6,7 +6,59 @@ namespace Delegatedemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter some text: ");
+            String text = Console.ReadLine();
+
+            //DispStrDelegate saying1 = new DispStrDelegate(Capitalize);
+            //DispStrDelegate saying2 = new DispStrDelegate(LowerCased);
+            //DispStrDelegate saying3 = new DispStrDelegate(Console.WriteLine);
+
+            //saying1(text);
+            //saying2(text);
+            //saying3(text);
+
+            //multi cast delegates
+            DispStrDelegate sayings = new DispStrDelegate(Capitalize);
+            sayings += new DispStrDelegate(LowerCased);
+            sayings += new DispStrDelegate(Console.WriteLine);
+
+            //Console.WriteLine("Running multi cast directly: ");
+            //sayings(text);
+
+            //Console.WriteLine("Running by passing delegate to another method: ");
+            //RunMyDelegate(sayings, text);
+
+            //Console.WriteLine("Running by passing in a lambda expression: ");
+            //RunMyDelegate((string t) => { Console.WriteLine("Lambda: " + t); }, text);
+
+            //Console.WriteLine("Lambda without type: ");
+            //RunMyDelegate((t) => { Console.WriteLine("Lambda: " + t); }, text);
+
+            //Console.WriteLine("Lambda without parenthesis: ");
+            //RunMyDelegate(t => { Console.WriteLine("Lambda2: " + t); }, text);
+
+            sayings += t => { Console.WriteLine("Lambda3: " + t); };
+            Console.WriteLine("Three Delegates and a lambda: ");
+            RunMyDelegate(sayings, text);
         }
+
+        delegate void DispStrDelegate(string param);
+
+        static void RunMyDelegate(DispStrDelegate del, string textParam)
+        {
+            del(textParam);
+        }
+
+        static void Capitalize(string text)
+        {
+            Console.WriteLine("Your input capatilized -->" + text.ToUpper());
+
+        }
+
+        static void LowerCased(string text)
+        {
+            Console.WriteLine("Your Input lower cased -->" + text.ToLower());
+        }
+
     }
 }
